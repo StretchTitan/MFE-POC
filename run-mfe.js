7a1a9e4ba33c9ef1937173438c09192a8015a3a6
Christopher Breit <christopher.breit@charter.com>
2020-05-13 08:43:54 -0600

    update to pass env parameter and readme

--- run-mfe.js
+++ run-mfe.js
@@ -1,15 +1,5 @@
 var fs = require("fs");
-var program = require("commander");
 var cp = require("child_process");
-
-console.log("Building all MFEs!");
-
-program
-  .arguments("<env>")
-  .action(function (env) {
-    console.log("environment: %s", env);
-  })
-  .parse(process.argv);
 
 var outputHandler = (error, stdout, stderr) => {
   if (error) {
@@ -35,38 +25,41 @@ var readOutput = (filename, mfe) => {
   });
 };
 
+var env = process.argv[2];
+var prodExt = env === "prod" ? ":prod" : "";
 var buildsComplete = {
-    homepage: false,
-    billing: false,
-    wrapper: false
+  homepage: false,
+  billing: false,
+  wrapper: false,
 };
 
-cp.exec('cd ../MFE-POC-HOMEPAGE && npm run build 2>&1 | tee homepage-build-log.txt', outputHandler);
-cp.exec('cd ../MFE-POC-HOMEPAGE && npm start 2>&1 | tee homepage-server-log.txt', outputHandler);
-console.log('Building homepage...');
+console.log("Building all MFEs!");
 
-cp.exec('cd ../mfe-poc-billing && npm run build 2>&1 | tee billing-build-log.txt', outputHandler);
-cp.exec('cd ../mfe-poc-billing && npm start 2>&1 | tee billing-server-log.txt', outputHandler);
-console.log('Building billing...');
+cp.exec(`cd ../MFE-POC-HOMEPAGE && npm run build${prodExt} 2>&1 | tee homepage-build-log.txt`, outputHandler);
+cp.exec("cd ../MFE-POC-HOMEPAGE && npm start 2>&1 | tee homepage-server-log.txt", outputHandler);
+console.log("Building homepage...");
 
-cp.exec('cd ../MFE-POC && npm start 2>&1 | tee wrapper-log.txt', outputHandler);
-console.log('Building wrapper...');
+cp.exec(`cd ../mfe-poc-billing && npm run build${prodExt} 2>&1 | tee billing-build-log.txt`, outputHandler);
+cp.exec("cd ../mfe-poc-billing && npm start 2>&1 | tee billing-server-log.txt", outputHandler);
+console.log("Building billing...");
+
+cp.exec(`cd ../MFE-POC && npm run start${prodExt} 2>&1 | tee wrapper-log.txt`, outputHandler);
+console.log("Building wrapper...");
 
 var counter = 0;
 var reader = setInterval(() => {
-    readOutput( __dirname + "/../MFE-POC-HOMEPAGE/homepage-build-log.txt", 'homepage');
-    readOutput( __dirname + "/../mfe-poc-billing/billing-build-log.txt", 'billing');
-    readOutput( __dirname + "/../MFE-POC/wrapper-log.txt", 'wrapper');
+  var { homepage, billing, wrapper } = buildsComplete;
 
-    var { homepage, billing, wrapper } = buildsComplete;
+  readOutput(__dirname + "/../MFE-POC-HOMEPAGE/homepage-build-log.txt", "homepage");
+  readOutput(__dirname + "/../mfe-poc-billing/billing-build-log.txt", "billing");
+  readOutput(__dirname + "/../MFE-POC/wrapper-log.txt", "wrapper");
 
+  if ((homepage && billing && wrapper) || counter > 60) {
+    clearInterval(reader);
+    console.log("all mfes have been built");
+  } else if (!(homepage || billing || wrapper)) {
+    console.log("...");
+  }
 
-    if ((homepage && billing && wrapper) || counter > 60) {
-        clearInterval(reader);
-        console.log('all mfes have been built');
-    } else if (!(homepage || billing || wrapper)) {
-        console.log('...');
-    }
-
-    counter++;
+  counter++;
 }, 1000);
